a6bfbbf1da9c02a99d0f78f17531e57d73fcd9b2
jaysuthar064 <jayantisuthar094@gmail.com>
2026-03-05 16:55:49 +0530

    test commit

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -5,7 +5,7 @@ import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/register",registerUser);
-router.post("/login",loginUser);
+router.post("/login",loginUser );
 
 router.get("/profile",protect,(req,res)=>{
     res.json({
