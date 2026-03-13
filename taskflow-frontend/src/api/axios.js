c2425f7ed86ba397976dd0e9c72dc962faba97fb
jaysuthar064 <jayantisuthar094@gmail.com>
2026-03-13 17:22:54 +0530

    fixes bugs

--- taskflow-frontend/src/api/axios.js
+++ taskflow-frontend/src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config()
 
 const API = axios.create({
-    baseURL : process.env.VITE_API_URL
+    baseURL : import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1"
 });
 
 API.interceptors.request.use((config)=>{
