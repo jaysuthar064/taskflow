a9af2350f18f5a0681e3f77c53ba9f4468ba99c1
jaysuthar064 <jayantisuthar094@gmail.com>
2026-03-15 21:59:16 +0530

    polished ui and ux

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -9,7 +9,7 @@ passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_SECRET_KEY,
+      clientSecret: process.env.GOOGLE_SECRET_KEY || process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.CALLBACK_URL || "/api/v1/auth/google/callback",
       proxy: true,
     },
